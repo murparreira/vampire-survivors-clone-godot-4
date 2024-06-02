ffa23bb283b011a6f76a4db3589230419527fa10
Murillo Parreira <murparreira@gmail.com>
2024-06-02 20:25:07 -0300

    improve: end screen ui

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -2,14 +2,21 @@ extends CanvasLayer
 
 class_name EndScreen
 
+@onready var panel_container = $%PanelContainer
+
 func _ready():
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	$%RestartButton.pressed.connect(on_restart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
 
 func set_defeat_labels():
 	$%TitleLabel.text = "DEFEAT"
-	$%SubtitleLabel.text = "YOU LOST!"
+	$%SubtitleLabel.text = "YOU DIED!"
 
 func on_restart_button_pressed():
 	get_tree().paused = false

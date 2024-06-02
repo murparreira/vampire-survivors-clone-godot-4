de81705a91c88df8418eec359fef86fbfcc6f457
Murillo Parreira <murparreira@gmail.com>
2024-06-02 11:08:16 -0300

    add: hit flash on damage to enemies

---
+++ scenes/components/hit_flash_component.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var health_component: HealthComponent
+@export var sprite: Sprite2D
+@export var hit_flash_material : ShaderMaterial
+
+var hit_flash_tween: Tween
+
+func _ready():
+	sprite.material = hit_flash_material
+	health_component.health_changed.connect(on_health_changed)
+
+func on_health_changed():
+	if hit_flash_tween != null && hit_flash_tween.is_valid():
+		hit_flash_tween.kill()
+
+	var shader = sprite.material as ShaderMaterial
+	shader.set_shader_parameter("lerp_percent", 1.0)
+	hit_flash_tween = create_tween()
+	hit_flash_tween.tween_property(shader, "shader_parameter/lerp_percent", 0.0, 0.25).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)

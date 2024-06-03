68f25bbca60fc4b813b0d04002da723819707d34
Murillo Parreira <murparreira@gmail.com>
2024-06-03 16:15:08 -0300

    add: pause menu

---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,49 @@
+extends CanvasLayer
+
+var is_closing
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
+@onready var panel_container = $%PanelContainer
+
+func _ready():
+	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2
+	$AnimationPlayer.play("default")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3).from(Vector2.ZERO).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
+	$%ResumeButton.pressed.connect(on_resume_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitButton.pressed.connect(on_quit_pressed)
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		close()
+		get_tree().root.set_input_as_handled()
+		
+func close():
+	if is_closing:
+		return
+		
+	is_closing = true
+	$AnimationPlayer.play_backwards("default")
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, .3).from(Vector2.ONE).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	await tween.finished
+	get_tree().paused = false
+	queue_free()
+
+func on_resume_pressed():
+	close()
+
+func on_options_pressed():
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+	
+func on_quit_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()

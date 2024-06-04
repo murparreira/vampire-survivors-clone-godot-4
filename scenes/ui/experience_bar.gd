7b4e8bfa3cb768dda998ac81ef5d650aa110264c
Murillo Parreira <murparreira@gmail.com>
2024-06-03 22:34:18 -0300

    add: level indicator on bottom left screen

--- scenes/ui/experience_bar.gd
+++ scenes/ui/experience_bar.gd
@@ -1,11 +1,16 @@
 extends CanvasLayer
 
 @export var experience_manager: ExperienceManager
-@onready var progress_bar = $MarginContainer/ProgressBar
+@onready var progress_bar = $%ProgressBar
+@onready var level_label = %LevelLabel
 
 func _ready():
 	experience_manager.experience_updated.connect(on_experience_updated)
+	experience_manager.level_up.connect(on_level_up)
 
 func on_experience_updated(current_experience: float, target_experience: float):
 	var percent = current_experience / target_experience
 	progress_bar.value = percent
+
+func on_level_up(new_level: int):
+	level_label.text = "Lvl. " + str(new_level)
